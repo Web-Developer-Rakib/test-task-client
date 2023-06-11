702d01b098717357a2f02acc8220df9203ada95a
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-06-11 08:39:27 +0600

    Get data component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import GetData from "./components/GetData/GetData";
+import PostData from "./components/PostData/PostData";
 
 function App() {
+  const [name, setName] = useState("");
+  const [selectedSectors, setSelectedSectors] = useState([]);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSectorChange = (e) => {
+    const { options } = e.target;
+    const selectedValues = [];
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        selectedValues.push(options[i].innerText.trim());
+      }
+    }
+    setSelectedSectors((prev) => [...prev, selectedValues]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Save the data to the database or perform any desired action
+    console.log("Name:", name);
+    console.log("Selected Sectors:", selectedSectors.flat());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <PostData
+        handleNameChange={handleNameChange}
+        handleSectorChange={handleSectorChange}
+        handleSubmit={handleSubmit}
+        name={name}
+        selectedSectors={selectedSectors}
+      />
+      <GetData />
     </div>
   );
 }
---
+++ src/components/GetData/GetData.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GetData.css";
+
+const GetData = () => {
+  return (
+    <div className="data-box">
+      <h3>name:</h3>
+    </div>
+  );
+};
+
+export default GetData;
